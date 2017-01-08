87650d36719e94a9c1586ed7075521f00baaecc7
Shawn Vader <svader@gmail.com>
2017-01-08 11:16:40 +0000

    Inital commit of our TodoApp

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 // Load foundation
 
 // Start foundation select document using jquery and call the foundation method
@@ -11,4 +13,4 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate 3 project</p>, document.getElementById('app'));
+  <TodoApp/>, document.getElementById('app'));
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (<div>todo TodoApp.jsx</div>);
+  }
+});
+
+module.exports = TodoApp;
