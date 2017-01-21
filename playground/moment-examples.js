8ebf100bb47ec8cc50581fdd83fdc5a17b0171a1
Shawn Vader <svader@gmail.com>
2017-01-21 06:55:30 +0000

    Add completed timestamps

---
+++ playground/moment-examples.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+console.log(moment().format());
+
+var now = moment();
+
+console.log('Current timestamp ' +  now);
+
+let timestamp = now.unix();
+
+console.log('Current date ' +  now.unix(timestamp));
+console.log('Current date ' +  moment.unix(timestamp).format('MMMM D, YYYY @ kk:mm'));
---
+++ playground/spread.js
@@ -0,0 +1,28 @@
+var person = ['Shawn', 22];
+var personTwo = ['Andrew', 25];
+
+//Hi x you are
+
+
+var names = ['Rajendra', 'Shawn'];
+var final = ['Ian'];
+
+// Hi
+
+
+function greeting(name, age) {
+  console.log(`Hi ${name}, you are ${age}`);
+}
+
+greeting(...person);
+greeting(...personTwo);
+
+function displayNames(names) {
+  names.forEach( (e) => {
+    console.log(`Hi ${e} `)
+  });
+}
+
+final = [...final, ...names];
+displayNames(final);
+
